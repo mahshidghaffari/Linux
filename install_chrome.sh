5d5672f90d9c5fcc8aa462daf12e0f0afd9fdbbb
mahshidghaffari <m.ghaffari@student.maastrichtuniversity.nl>
2023-05-02 18:27:46 +0200

    Added chrome

---
+++ install_chrome.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~/Downloads
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+rm -rf google-chrome-stable_current_amd64.deb
